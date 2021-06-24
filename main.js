b0cfae2459471b12fd6b3a20577db22dda27f525
Parvathi-Jayapal <77677826+Parvathi-Jayapal@users.noreply.github.com>
2021-06-24 20:20:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function lipstickPage() {
+  window.location = "lipstick.html" ;
+}
+function whiskerPage()
+{
+  window.location = "whiskers.html" ;
+}
+function earringPage()
+{
+  window.location = "earrings.html" ;   
+}
